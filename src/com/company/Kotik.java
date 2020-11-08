10adb51998d128cc00f332f42a25f4c5fb88e851
Valeria Martynova <valeriaa.martynova@yandex.ru>
2020-11-08 13:38:37 +0300

    add overloading

--- src/com/company/Kotik.java
+++ src/com/company/Kotik.java
@@ -1,66 +1,110 @@
 package com.company;
 
+
 class Kotik {
-    int satiety = 70;
+    int satiety = 100;
 
     void play() {
-        if (satiety < 70) {
+        if (satiety < 50) {
             System.out.println("i want to eat");
+            eat();
         } else {
             System.out.println("play");
+            satiety = satiety - 10;
         }
     }
 
     void sleep() {
-        if (satiety < 70) {
+        if (satiety < 50) {
             System.out.println("i want to eat");
+            eat();
         } else {
             System.out.println("sleep");
+            satiety = satiety - 10;
         }
     }
 
     void chaseMouse() {
-        if (satiety < 70) {
+        if (satiety < 50) {
             System.out.println("i want to eat");
+            eat();
         } else {
-        System.out.println("chaseMouse");
-
+            System.out.println("chaseMouse");
+            satiety = satiety - 10;
        }
     }
 
-    void eat(){
-        if (satiety < 70) {
-            System.out.println("i want to eat");
-        } else {
-            System.out.println("eat");
-        }
+    void eat(int satietyFrom, String food){
+        System.out.println("We fed a cat, food: " + food + ", satiety: " + satietyFrom);
+        satiety = satietyFrom;
+    }
+
+    void eat(int satietyFrom) {
+        System.out.println("We fed a cat" + ", satiety: " + satietyFrom);
+        satiety = satietyFrom;
+
+//        eat(satietyFrom, "fish");
+    }
+
+    void eat() {
+        eat(100, "fish");
     }
 
     void jump(){
-        if (satiety < 70) {
+        if (satiety < 50) {
             System.out.println("i want to eat");
+            eat();
         } else {
             System.out.println("jump");
+            satiety = satiety - 10;
         }
     }
 
     void sayMeow(){
-        if (satiety < 70) {
+        if (satiety < 50) {
             System.out.println("i want to eat");
+            eat();
         } else {
             System.out.println("sayMeow");
+            satiety = satiety - 10;
         }
     }
 
     void liveAnotherDay(){
-        for ( int count = 24;count>0;count = count - 1){
-            play();
-            sleep();
-            chaseMouse();
-            eat();
-            jump();
-            sayMeow();
+        int from = 1;
+        int to = 6;
 
-       }
+        for ( int count = 24; count > 0; count = count - 1){
+            double random = Math.random();
+            int random_number1 = from + (int) (random * to);
+
+//            System.out.println(random_number1);
+
+            switch (random_number1) {
+                case 1:
+                    play();
+                    break;
+                case 2:
+                    sleep();
+                    break;
+                case 3:
+                    chaseMouse();
+                    break;
+//                case 4:
+//                    eat();
+//                    break;
+                case 5:
+                    jump();
+                    break;
+                case 6:
+                    sayMeow();
+                    break;
+                default:
+                    break;
+            }
+
+
+
+        }
     }
 }
